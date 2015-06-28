39c84ea2ea5178b8cfcb87b9899b80fc4868ec0f
sugilog <sugilog@gmail.com>
2015-06-28 21:09:30 +0900

    output plugin

---
+++ lib/fluent/plugin/out_nested_hash.rb
@@ -0,0 +1,28 @@
+require "fluent/plugin/nested_hash_filter/nested_object"
+
+module Fluent
+  class OutNestedHash < Output
+    Plugin.register_output("out_nested_hash", self)
+
+    def configure conf
+      super
+    end
+
+    def start
+      super
+    end
+
+    def shutdown
+      super
+    end
+
+    def emit tag, es, chain
+      es.each do |time, record|
+        record = Fluent::NestedHashFilter::NestedObject.convert record
+        Fluent::Engine.emit tag, time, record
+      end
+
+      chain.next
+    end
+  end
+end
--- test/fluent/plugin/out_nested_hash_test.rb
+++ test/fluent/plugin/out_nested_hash_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
-require "fluent/plugin/filter_nested_hash"
+require "fluent/plugin/out_nested_hash"
 
-class FilterNestedHashTest < Test::Unit::TestCase
+class OutNestedHashTest < Test::Unit::TestCase
   include Fluent
 
   setup do
@@ -9,61 +9,62 @@ class FilterNestedHashTest < Test::Unit::TestCase
     @time = Fluent::Engine.now
   end
 
-  def create_driver config = ""
-    driver = Test::FilterTestDriver.new FilterNestedHash
-    driver.configure config, true
+  def create_driver config = "", tag = "test"
+    driver = Fluent::Test::OutputTestDriver.new OutNestedHash, tag
+    driver.configure config
   end
 
-  def emit config, messages
-    driver = create_driver config
+  def emit config, tag, messages
+    driver = create_driver config, tag
 
     driver.run do
       messages.each do |message|
-        driver.emit message, @time
+        driver.emit message
       end
     end
-    
+
     driver
   end
 
-  def filtered driver, index
-    result = driver.filtered_as_array[index]
+  def emitted driver, index
+    result = driver.emits[index]
     {tag: result[0], time: result[1], message: result[2]}
   end
 
   sub_test_case "configure" do
-    test "check default" do
-      assert_nothing_raised do
-        create_driver "default"
-      end
+    test "check_default" do
+      create_driver "default"
     end
   end
 
-  sub_test_case "filter" do
+  sub_test_case "emit" do
     test "with valid record" do
-      driver = emit "", [
+      driver = emit "", "test", [
         {a: 1, b: {c: 2, d: {e: 3, f:4}, g: [10, 20, 30]}, h: [], i: {}},
         {a: {b: {c: 1, d: {e: 2, f:3}, g: [10, 20, 30]}, h: [], i: {}}}
       ]
 
       expect_message = {"a" => 1, "b.c" => 2, "b.d.e" => 3, "b.d.f" => 4, "b.g.0" => 10, "b.g.1" => 20, "b.g.2" => 30, "h" => nil, "i" => nil}
-      result = filtered driver, 0
+      result = emitted driver, 0
+      assert_equal "test", result[:tag]
       assert_equal expect_message, result[:message]
 
       expect_message = {"a.b.c" => 1, "a.b.d.e" => 2, "a.b.d.f" => 3, "a.b.g.0" => 10, "a.b.g.1" => 20, "a.b.g.2" => 30, "a.h" => nil, "a.i" => nil}
-      result = filtered driver, 1
+      result = emitted driver, 1
+      assert_equal "test", result[:tag]
       assert_equal expect_message, result[:message]
     end
 
     test "with invalid record" do
-      driver = emit "", ["message", {hoge: 1, fuga: {"test0" => 2, "test1" => 3}}]
+      driver = emit "", "test", ["message", {hoge: 1, fuga: {"test0" => 2, "test1" => 3}}]
 
       expect_message = {}
-      result = filtered driver, 0
+      result = emitted driver, 0
       assert_equal expect_message, result[:message]
 
       expect_message = {"hoge" => 1, "fuga.test0" => 2, "fuga.test1" => 3}
-      result = filtered driver, 1
+      result = emitted driver, 1
+      assert_equal "test", result[:tag]
       assert_equal expect_message, result[:message]
     end
   end
